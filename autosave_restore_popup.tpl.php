a628207c131a26c33305e4af2aade6eaa4d1fa4f
CZÖVEK András <czigor@freemail.hu>
2014-07-10 23:39:19 +0200

    Issue #2009766: Proper t() usage

--- autosave_restore_popup.tpl.php
+++ autosave_restore_popup.tpl.php
@@ -6,7 +6,7 @@
 ?>
 
 <span id="status">
-  <?php print t('This form was autosaved on ' . $autosave['savedDate']); ?>
+  <?php print t('This form was autosaved on @date', array('@date' => $autosave['savedDate'])); ?>
 </span>
 <span id="operations">
   <?php print $ignore_link . $restore_link; ?>
